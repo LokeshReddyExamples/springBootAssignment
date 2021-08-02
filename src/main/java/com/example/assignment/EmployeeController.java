5b58b772e31331dc783cdfdbfd7b8200051d99ce
Lokesh <Lokesh@SYSBCP1880.emids.local>
2021-08-02 13:18:01 +0530

    code changes made

--- src/main/java/com/example/assignment/EmployeeController.java
+++ src/main/java/com/example/assignment/EmployeeController.java
@@ -1,7 +1,13 @@
 package com.example.assignment;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +18,38 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService employeeService;
 	
-	@PostMapping(path="/create")
+	@PostMapping("/create")
 	public @ResponseBody Employee create(@RequestBody Employee employee) {
 		return employeeService.save(employee);
 	}
-
+	
+	@PutMapping("/update/{id}")
+	public Employee  update(@RequestBody Employee latestEmployee,@PathVariable int id) {
+		return employeeService.findById(id)
+		.map(employee -> {
+			employee.setFirstName(latestEmployee.getFirstName());
+			employee.setLastName(latestEmployee.getLastName());
+			employee.setEmailId(latestEmployee.getEmailId());
+			return employeeService.save(employee);
+		}).orElseGet(() -> {
+			latestEmployee.setId(id);
+	        return employeeService.save(latestEmployee);
+	      });
+	}
+		
+	@GetMapping("/getEmployee/{id}")
+	  public Employee getEmployee(@PathVariable int id) {
+	    return employeeService.findById(id).get();
+	  }
+	
+	@GetMapping("getAllEmployees")
+	public List<Employee> getAllEmployees(){
+		return employeeService.getAllEmployees();
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public void deleteEmployee(@PathVariable int id) {
+		employeeService.deleteById(id);
+	}
 	
 }
--- src/main/java/com/example/assignment/EmployeeService.java
+++ src/main/java/com/example/assignment/EmployeeService.java
@@ -1,5 +1,8 @@
 package com.example.assignment;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +14,18 @@ public class EmployeeService {
 	
 	public Employee save (Employee employee) {
 		return repository.save(employee);
+	}
+	
+	public Optional<Employee> findById(int id) {
+		return repository.findById(id);
+	}
+	
+	public List<Employee> getAllEmployees() {
+		return repository.findAll();
+	}
+	
+	public void deleteById(int id) {
+		 repository.deleteById(id);
 	}
 
 }

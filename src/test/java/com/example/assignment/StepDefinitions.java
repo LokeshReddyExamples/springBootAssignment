3c90f7e4fe9179621e65aac3c19d9433f1be0d74
Lokesh <Lokesh@emids.com>
2021-09-02 11:13:04 +0530

    Renaming method names

--- src/test/java/com/example/assignment/StepDefinitions.java
+++ src/test/java/com/example/assignment/StepDefinitions.java
@@ -40,13 +40,13 @@ public class StepDefinitions {
 	private HttpClientErrorException exception;
 
 	@Given("passing valid employee record")
-	public void passing_valid_employee_record() {
+	public void passingValidEmployeeRecord() {
 		expectedObject.setEmailId("joy@gmail.com");
 		expectedObject.setFirstName("joy");
 		expectedObject.setLastName("R");
 	}
 	@When("employee should successfully saved to db")
-	public void employee_should_successfully_saved_to_db() {
+	public void employeeShouldSuccessfullySavedToDb() {
 		actualResult = employeeHttpClient.put(expectedObject);
 	}
 	@Then("it should return saved employee object")
@@ -55,52 +55,52 @@ public class StepDefinitions {
 	}
 
 	@When("employee id is {int}")
-	public void employee_id_is(Integer id) {
+	public void employeeIdIs(Integer id) {
 			ResponseEntity responseEntity = employeeHttpClient.getContents(id);
 			actualResult = (Employee) responseEntity.getBody();
 	}
 	@Then("it should return the employee record")
-	public void it_should_return_the_employee_record() {
+	public void itShouldReturnTheEmployeeRecord() {
 		assertEquals(36,actualResult.getId());
 	}
 
 	@When("passing invalid employee id")
-	public void passing_invalid_employee_id() {
+	public void passingInvalidEmployeeId() {
 		 exception = assertThrows(HttpClientErrorException.class,() ->{
 			employeeHttpClient.getContents(-1);
 		});
 
 	}
 	@Then("it should return empty employee record")
-	public void it_should_return_empty_employee_record() throws JsonProcessingException {
+	public void itShouldReturnEmptyEmployeeRecord() throws JsonProcessingException {
 		assertEquals(HttpStatus.NOT_FOUND,exception.getStatusCode());
 
 	}
 
 	@Then("it should return all the employees")
-	public void it_should_return_all_the_employees() {
+	public void itShouldReturnAllTheEmployees() {
 			List<Employee> list = employeeHttpClient.getContents();
 			Assert.assertTrue(list instanceof List);
 	}
 
 	@When("passing latest employee with valid id")
-	public void passing_latest_employee_with_valid_id() {
+	public void passingLatestEmployeeWithValidId() {
 		expectedObject.setFirstName("update_f_name");
 		expectedObject.setLastName("update_l_name");
 		employeeHttpClient.put(expectedObject,88);
 	}
 	@Then("it should update with latest employee record for same id")
-	public void it_should_update_with_latest_employee() {
+	public void itShouldUpdateWithLatestEmployee() {
 			assertEquals(expectedObject.getFirstName(),((Employee)employeeHttpClient.getContents(88).getBody()).getFirstName());
 	}
 
 	@When("passing valid employee id {int}")
-	public void passing_valid_employee_id(Integer id) {
+	public void passingValidEmployeeId(Integer id) {
 			employeeHttpClient.delete(id);
 	}
 
 	@Then("it should delete employee with id {int}")
-	public void it_should_delete_employee_with_id(Integer empId) {
+	public void itShouldDeleteEmployeeWithId(Integer empId) {
 		assertFalse(employeeHttpClient.getContents().stream()
 				 .anyMatch(employee -> employee.getId()==empId));
 		}

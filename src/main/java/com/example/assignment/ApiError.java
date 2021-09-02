f9575e8406fcf53270f1e0060faf740a1f325f2d
Lokesh <Lokesh@emids.com>
2021-09-02 11:23:47 +0530

    Using lambok for get/set methods

--- src/main/java/com/example/assignment/ApiError.java
+++ src/main/java/com/example/assignment/ApiError.java
@@ -1,29 +1,14 @@
 package com.example.assignment;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
+@Getter
+@Setter
 public class ApiError {
 
     private HttpStatus status;
     private String message;
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-
-
-    public HttpStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(HttpStatus status) {
-        this.status = status;
-    }
-
 
 }
